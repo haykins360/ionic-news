dd7b480355bdb8db4c13c3d60e20ba935b17451c
haykins <akinsholasamuel@gmail.com>
2017-02-10 12:47:09 +0100

    Commiting all files to git repo

---
+++ www/js/app.js
@@ -0,0 +1,69 @@
+(function() {
+
+	var app = angular.module('newsaggregator', ['ionic', 'angularMoment']);
+	
+	app.controller('AggregatorCtrl', function($http, $scope) {
+	
+		$scope.stories	=	[];
+		
+		// $http.get('https://www.reddit.com/r/Android/new/.json')
+		// 	.success(function(response){
+		// 		//console.log(response.data.children);
+		// 		angular.forEach(response.data.children, function(child){
+		// 			//console.log(child.data);
+		// 			$scope.stories.push(child.data);
+		// 		});
+		// 	});
+
+		function loadStories(params, callback) {
+
+			$http.get('https://www.reddit.com/r/Android/new/.json', {params: params})
+			.success(function(response){
+				var stories = [];
+				angular.forEach(response.data.children, function(child){
+					$scope.stories.push(child.data);
+				});
+				callback(stories);
+			});
+
+		}
+
+		$scope.loadOlderStories	= function () {
+
+			var params = {};
+			if ($scope.stories.length > 0) {
+				params['after'] = $scope.stories[$scope.stories.length - 1].name;
+			}
+
+			loadStories(params, function (OlderStories) {
+				$scope.stories = $scope.stories.concat(OlderStories);
+				$scope.$broadcast('scroll.infiniteScrollComplete');
+			});
+
+		};
+
+		$scope.loadNewerStories = function (argument) {
+			var params = {'before': $scope.stories[0].name};
+			loadStories(params, function (newerStories) {
+				$scope.stories = newerStories.concat($scope.stories);
+				$scope.$broadcast('scroll.refreshComplete');
+			})
+		};
+		
+	});
+
+	app.run(function($ionicPlatform) {
+	  $ionicPlatform.ready(function() {
+	    if(window.cordova && window.cordova.plugins.Keyboard) {
+
+	      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+	      cordova.plugins.Keyboard.disableScroll(true);
+	    }
+	    if(window.StatusBar) {
+	      StatusBar.styleDefault();
+	    }
+	  });
+	})
+	
+}());
